aa67b5b40922963fa1d18362c58d9ebee57c6d43
Sam Mansch <s.mansch@gmail.com>
2022-03-01 14:05:04 -0600

    Add functions to make game playable in console

--- script.js
+++ script.js
@@ -3,4 +3,42 @@ function computerPlay() {
     let computerMove = moves[Math.floor(Math.random()*moves.length)];
     return computerMove;
 }
-console.log(computerPlay());
+
+function playRound(playerSelection, computerSelection){
+    let playerMove = playerSelection.toLowerCase();
+
+    if (playerMove === "rock" && computerSelection === "paper") {
+        return "You lose! Paper beats rock";
+    }
+    else if (playerMove === "rock" && computerSelection === "scissors") {
+        return "You win! Rock beats scissors"
+    } 
+    else if (playerMove === "paper" && computerSelection === "rock") {
+        return "You win! Paper beats rock";
+    }
+    else if (playerMove === "paper" && computerSelection === "scissors") {
+        return "You lose! Scissors beat paper"
+    } 
+    else if (playerMove === "scissors" && computerSelection === "paper") {
+        return "You win! Scissors beat paper";
+    }
+    else if (playerMove === "scissors" && computerSelection === "rock") {
+        return "You lose! Rock beats scissors"
+    } else {
+        return "Draw!"
+    }
+}
+
+function game() {
+    for (let i = 0; i < 5; i++ ){
+        let playerSelection = window.prompt("Choose 'rock', 'paper', or 'scissors':")
+        let computerSelection = computerPlay();
+
+        var result = playRound(playerSelection, computerSelection);
+
+        console.log(result);
+        console.log(playerSelection, computerSelection);
+
+    }
+}
+console.log(game());
