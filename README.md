acc21f15d24d48a7d4d3b30ebf585ad319e9d877
Sam Mansch <s.mansch@gmail.com>
2022-03-01 14:49:54 -0600

    Rename script.js to main.js and add information to README

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # rock-paper-scissors
+
+---
+
+This project was done as part of [The Odin Project's](https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/rock-paper-scissors) Fundamentals path.
+
+This initial version is a basic game playable in the browser console.
+
+concepts practiced in the initial build:
+
+- functions
+- input from prompts
+- alert
+- differently scoped variables
+- if/else
+- for loops
+- template literals
+
+:heavy_exclamation_mark:Coming soon! A Rock, Paper, Scissors GUI:heavy_exclamation_mark:
--- main.js
+++ main.js
@@ -8,26 +8,35 @@ function playRound(playerSelection, computerSelection){
     let playerMove = playerSelection.toLowerCase();
 
     if (playerMove === "rock" && computerSelection === "paper") {
+        computerScore += 1;
         return "You lose! Paper beats rock";
     }
     else if (playerMove === "rock" && computerSelection === "scissors") {
+        playerScore += 1;
         return "You win! Rock beats scissors"
     } 
     else if (playerMove === "paper" && computerSelection === "rock") {
+        playerScore += 1;
         return "You win! Paper beats rock";
     }
     else if (playerMove === "paper" && computerSelection === "scissors") {
+        computerScore += 1;
         return "You lose! Scissors beat paper"
     } 
     else if (playerMove === "scissors" && computerSelection === "paper") {
+        playerScore += 1;
         return "You win! Scissors beat paper";
     }
     else if (playerMove === "scissors" && computerSelection === "rock") {
+        computerScore += 1;
         return "You lose! Rock beats scissors"
     } else {
         return "Draw!"
     }
 }
+
+let playerScore = 0;
+let computerScore = 0;
 
 function game() {
     for (let i = 0; i < 5; i++ ){
@@ -37,8 +46,15 @@ function game() {
         var result = playRound(playerSelection, computerSelection);
 
         console.log(result);
-        console.log(playerSelection, computerSelection);
+        console.log(playerScore, computerScore);
 
+    }
+    if (playerScore > computerScore) {
+        alert(`Congratulations! You win!\n\nFinal Score:\nYou: ${playerScore} Computer: ${computerScore}`);
+    } else if (playerScore == computerScore) {
+        alert(`Looks like a draw!\n\nFinal Score:\nYou: ${playerScore} Computer: ${computerScore}`)
+    } else {
+        alert(`Better luck next time\n\nFinal Score:\nYou: ${playerScore} Computer: ${computerScore}`);
     }
 }
 console.log(game());
